d4562be57c363f388e244907b5e66138f95ce24f
Peter Tripp <petertripp@gmail.com>
2023-08-02 17:21:18 -0400

    Working stubs. No types.

--- doc_scraper/src/main.rs
+++ doc_scraper/src/main.rs
@@ -1,5 +1,35 @@
+#![allow(dead_code)]
+
 use std::{path::Path, fs::File, io::Read};
 use regex::Regex;
+use scraper::Selector;
+
+#[derive(Debug)]
+struct Stub {
+    title: String,
+    anchor: String,
+    text: Vec<String>,
+}
+
+impl Stub {
+    fn to_stub(&self) -> String {
+        String::from(
+            format!("function {} end\n", self.title)
+        )
+    }
+    fn to_lua(&self) -> String {
+        String::from(
+            format!("{}--- https://sdk.play.date/2.0.1/Inside%20Playdate.html#{}\n{}\n", self.text2comments(), self.anchor, self.to_stub())
+        )
+    }
+    fn text2comments (&self) -> String {
+        let mut s = String::new();
+        for t in &self.text {
+            s.push_str(&format!("--- {}\n---\n", t));
+        }
+        s
+    }
+}
 
 fn main() {
     // let response = reqwest::blocking::get(
@@ -20,18 +50,18 @@ fn main() {
 
 
     let document = scraper::Html::parse_document(&response);
-    let outer = scraper::Selector::parse("div.sect1>div.sectionbody>div.sect2").unwrap();
+    let outer = Selector::parse("div.sect1>div.sectionbody>div.sect2").unwrap();
 
-    // let selector1 = scraper::Selector::parse("div.sect1>div.sectionbody>div.sect2>div.sect3>div.item").unwrap();
-    let selector1 = scraper::Selector::parse("div.sect3>div.item").unwrap();
-    let selector2 = scraper::Selector::parse("div.item").unwrap();
+    // let selector1 = Selector::parse("div.sect1>div.sectionbody>div.sect2>div.sect3>div.item").unwrap();
+    let selector1 = Selector::parse("div.sect3>div.item").unwrap();
+    let selector2 = Selector::parse("div.item").unwrap();
 
-    let sel_title = scraper::Selector::parse("div.title").unwrap();
-    let sel_content = scraper::Selector::parse("div.content").unwrap();
-    let sel_paragraph = scraper::Selector::parse("div.paragraph").unwrap();
-    let sel_p = scraper::Selector::parse("p").unwrap();
+    let sel_title = Selector::parse("div.title").unwrap();
+    let sel_content = Selector::parse("div.content").unwrap();
+    let sel_paragraph = Selector::parse("div.paragraph").unwrap();
+    let sel_p = Selector::parse("p").unwrap();
 
-    let sel_admonition = scraper::Selector::parse("div.admonitionblock>table>tbody>tr>td.content").unwrap();
+    let sel_admonition = Selector::parse("div.admonitionblock>table>tbody>tr>td.content").unwrap();
     let re_code = Regex::new(r"</?code>").unwrap();
     let re_em = Regex::new(r"</?em>").unwrap();
     let re_a = Regex::new(r"</?a[^>]*>").unwrap();
@@ -41,26 +71,28 @@ fn main() {
     let re_operator = Regex::new(r"(?:[\-\+\*\/]| \.\. )").unwrap();
     let re_brackets = Regex::new(r"[\[\]]").unwrap();
     let re_function = Regex::new(r"^(?:[\w][\w\d]*\.)*(?:[\w][\w\d]*)(?:[:.][\w][\w\d]*)").unwrap();
-    // let re_class = Regex::new(r"^((?:[\w][\w\d]*\.)*(?:[\w][\w\d]*))(?:[:][\w][\w\d]*)").unwrap();
 
     let mut _poop = 0;
     let mut _last_class: String = "".to_string();
+    let mut stubs : Vec<Stub> = Vec::new();
+
+
     for element in document.select(&outer) {
         for d2 in element.select(&selector1).chain(element.select(&selector2)) {
         // for d2 in element.select(&selector2) {
             _poop = _poop + 1;
 
             let anchor = d2.value().attr("id").unwrap_or("");
-
             let mut title = d2.select(&sel_title).next().unwrap().text().collect::<String>();
+
             let mut text: Vec<String> = Vec::new();
             for c in d2.select(&sel_content) {
-                for div_p in c.select(&sel_paragraph) {
+                for div_p in c.select(&sel_paragraph) { // Paragraphs of the documentation
                     for p in div_p.select(&sel_p) {
                         text.push(p.text().collect::<String>());
                     }
                 }
-                for td in c.select(&sel_admonition) {
+                for td in c.select(&sel_admonition) { // Add admonitions parapgraphs
                     let adm = td.inner_html();
                     let a1 = re_code.replace_all(&adm, "`");
                     let a2 = re_em.replace_all(&a1, "*");
@@ -107,21 +139,20 @@ fn main() {
             if anchor == "" {
                 // println!("WARN: No id for: {}", title);
             }
-            // println!("-- {url}", url="https://sdk.play.date/2.0.1/Inside%20Playdate.html#".to_string() + &anchor);
-            println!("function {title} end")
-            // println!("  {}\n", text.join("\n  "));
-
-            // if _poop > 3 { panic!(); }
-
-            // let title = d2.select(&sel_title).next().unwrap().value();
-            // let content = d2.select(&sel_content).next().unwrap().text().collect::<String>();
-            // let name = "";
-            // println!("{} {} {}", anchor, title, content);
+            if title.contains("  ") { // Functions with multiple
+                for t in title.split("  ") {
+                    let stub = Stub { title: t.to_string().trim().to_owned(), anchor: anchor.to_string(), text: text.clone() };
+                    stubs.push(stub)
+                }
+            }
+            else {
+                stubs.push(Stub { title: title, anchor: anchor.to_string(), text: text });
+            }
         }
-        // println!("{}, {}", element.value().name(), element.value().attr("id").unwrap());
-        // let sub = scraper::Selector::parse("div.title").unwrap();
-            // println!("{}", element.select("div.title").collect::<String>());
-        //     print!("{} ", divs.text().collect::<String>());
+        for stub in &stubs {
+            println!("{}", stub.to_lua());
+            // println!("{}", stub.to_stub());
+        }
     }
     // titles
     //     .zip(1..101)
