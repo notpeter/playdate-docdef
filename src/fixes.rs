5aa06ccd975159cacdfb443634557fc36e1789d3
Peter Tripp <petertripp@gmail.com>
2023-08-05 15:01:39 -0400

    Loading Parmeter Types from TOML file. Load global list of valid types. Validate param types against global list.

--- src/fixes.rs
+++ src/fixes.rs
@@ -5,6 +5,24 @@ use lazy_static::lazy_static;
 use serde::Deserialize;
 
 
+static TOML_FIXES_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Fixes.toml"));
+static TOML_REPLACE_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Replace.toml"));
+static TOML_TYPES_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Types.toml"));
+
+lazy_static! {
+    static ref RE_CODE: Regex = Regex::new(r"</?code>").unwrap();
+    static ref RE_EM: Regex = Regex::new(r"</?em>").unwrap();
+    static ref RE_A: Regex = Regex::new(r"</?a[^>]*>").unwrap();
+    static ref RE_STRONG: Regex = Regex::new(r"</?strong>").unwrap();
+    static ref HTML_TAG: Regex = Regex::new(r"<[^>]*>").unwrap();
+    static ref LUA_FUNC: Regex = Regex::new(
+        &format!(r"^(?P<fname>(?:{id}\.)*{id}[:\.]{id}|{id})\((?P<params>.*)\)", id=r"[\w_][\w\d_]*").to_string()
+    ).unwrap();
+    static ref FIXES: HashMap<String, AnchorOverride> = load_fixes(&TOML_FIXES_STR);
+    static ref REPLACEMENTS: HashMap<String, String> = load_replacements(&TOML_REPLACE_STR);
+    static ref TYPES: TypesTOML = load_types(&TOML_TYPES_STR);
+}
+
 #[derive(Deserialize)]
 struct AnchorOverride {
     fname: String,
@@ -17,25 +35,10 @@ impl fmt::Display for AnchorOverride {
     }
 }
 
-
-static FIXES_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Fixes.toml"));
-static REPLACE_STR: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Replace.toml"));
-
-fn lua_function_regex() -> Regex {
-    let id = r"[\w_][\w\d_]*";
-    let f = format!(r"^(?P<fname>(?:{id}\.)*{id}[:\.]{id}|{id})\((?P<params>.*)\)");
-    Regex::new(&f).unwrap()
-}
-
-lazy_static! {
-    static ref RE_CODE: Regex = Regex::new(r"</?code>").unwrap();
-    static ref RE_EM: Regex = Regex::new(r"</?em>").unwrap();
-    static ref RE_A: Regex = Regex::new(r"</?a[^>]*>").unwrap();
-    static ref RE_STRONG: Regex = Regex::new(r"</?strong>").unwrap();
-    static ref HTML_TAG: Regex = Regex::new(r"<[^>]*>").unwrap();
-    static ref LUA_FUNC: Regex = lua_function_regex();
-    static ref FIXES: HashMap<String, AnchorOverride> = load_fixes(&FIXES_STR);
-    static ref REPLACEMENTS: HashMap<String, String> = load_replacements(&REPLACE_STR);
+#[derive(Deserialize)]
+struct TypesTOML {
+    types: HashMap<String, String>, // Array of valid types
+    by_name: HashMap<String, String>, // parameter_name=type_name
 }
 
 pub fn get_overrides(anchor: &str) -> Option<(String, Vec<String>)> {
@@ -49,6 +52,7 @@ pub fn get_overrides(anchor: &str) -> Option<(String, Vec<String>)> {
 
 // Takes a valid function signature and returns a function name and a vector of parameters.
 pub fn params_from_title(title: &String) -> (String, Vec<String>) {
+    let param_types = &TYPES.by_name;
     let mut params = Vec::new();
     let caps = LUA_FUNC.captures(title).unwrap();
 
@@ -100,12 +104,14 @@ fn clean_parameters(title: &String, params: &Vec<String>) -> Vec<String> {
     v
 }
 
+// Initialization functions run by lazy_static
+
 fn load_fixes(fixes_toml: &str) -> HashMap<String, AnchorOverride> {
     let overrides: HashMap<String, AnchorOverride> = match toml::from_str(fixes_toml) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("ERROR: {:?}", e);
-            HashMap::new()
+            println!("ERROR: Loading Fixes.toml failed.");
+            panic!("ERROR: {:?}", e);
         }
     };
     overrides
@@ -115,9 +121,27 @@ fn load_replacements(replace_toml: &str) -> HashMap<String, String> {
     let replacements: HashMap<String, String> = match toml::from_str(replace_toml) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("ERROR: {:?}", e);
-            HashMap::new()
+            println!("ERROR: Loading Replace.toml failed.");
+            panic!("ERROR: {:?}", e);
         }
     };
     replacements
+}
+
+fn load_types(types_toml: &str) -> TypesTOML {
+    let tt: TypesTOML = match toml::from_str(types_toml) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("ERROR: Loading Types.toml failed.");
+            panic!("ERROR: {:?}", e);
+        }
+    };
+
+    // validate all types in `by_name` are in `types` (catches typos)
+    for (type_name, type_type) in &tt.by_name {
+        if !tt.types.contains_key(type_type) {
+            panic!("Error: Found type name `{}` whose type `{}` is missing from type map Types.toml", type_name, type_type);
+        }
+    }
+    tt
 }
