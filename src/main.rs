6ba7e41adae5b29934bed54dc55288dd90ec461b
Peter Tripp <petertripp@gmail.com>
2023-09-21 13:36:37 -0400

    WIP. Maybe working.

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,13 @@ fn main() {
                 let mut iterator = line.into_inner();
                 let obj_name: &str = iterator.next().unwrap().as_str();
                 let obj_type: &str = iterator.next().unwrap().as_str();
-                let obj_proto: Vec<(&str, &str)> = Vec::new();
-                // Fixme: Actually parse fields.
-                // let proto = LuarsParser::parse(Rule::Document, iterator.next().unwrap().as_str());
+                let mut obj_proto: Vec<(&str, &str)> = Vec::new();
+                for field in iterator {
+                    let mut field_iterator = field.into_inner();
+                    let field_name: &str = field_iterator.next().unwrap().as_str();
+                    let field_type: &str = field_iterator.next().unwrap().as_str();
+                    obj_proto.push((field_name, field_type));
+                }
                 LuarsStatement::Object(obj_name, obj_type, obj_proto)
             }
             Rule::Variable => {
@@ -58,8 +62,23 @@ fn main() {
             Rule::Function => {
                 let mut iterator = line.into_inner();
                 let func_name: &str = iterator.next().unwrap().as_str();
-                let func_params: Vec<(&str, &str)> = Vec::new();
-                let func_returns: Vec<(&str, &str)> = Vec::new();
+                let mut func_params: Vec<(&str, &str)> = Vec::new();
+                if iterator.peek().unwrap().as_rule() != Rule::Return {
+                    let func_params_iterator = iterator.next().unwrap().into_inner();
+                    for param in func_params_iterator {
+                        let mut param_iterator = param.into_inner();
+                        let param_name: &str = param_iterator.next().unwrap().as_str();
+                        let param_type: &str = param_iterator.next().unwrap().as_str();
+                        func_params.push((param_name, param_type));
+                    }
+                }
+                let mut func_returns: Vec<(&str, &str)> = Vec::new();
+                let func_returns_iterator = iterator.next().unwrap().into_inner();
+                for ret in func_returns_iterator {
+                    if ret.as_rule() == Rule::TypeLua {
+                        func_returns.push(("", ret.as_str()));
+                    }
+                }
                 LuarsStatement::Function(func_name, func_params, func_returns)
             }
             _ => {
