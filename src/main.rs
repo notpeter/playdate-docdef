3b616ec99d88571f3cdf0c4e49dcec913b665d01
Peter Tripp <petertripp@gmail.com>
2023-09-21 11:20:48 -0400

    WIP.

--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,88 @@
-use regex::Regex;
-use scraper::{Selector, CaseSensitivity};
+use std::fs;
 
-mod args;
-mod class;
-mod config;
-mod fixes;
-mod stub;
+use pest::Parser;
+use pest_derive::Parser;
 
-use args::Action;
-use class::print_class_defs;
-use fixes::{clean_text, annotate_function};
-use stub::Stub;
+#[derive(Parser)]
+#[grammar = "luars.pest"]
+pub struct LuarsParser;
 
-use crate::fixes::clean_code;
+
+#[derive(Debug)]
+enum LuarsStatement<'a> {
+    Module(&'a str),
+    Variable(&'a str, &'a str),
+    Constant(&'a str, &'a str, isize),
+    Function(&'a str, Vec<(&'a str, &'a str)>),
+    Object(&'a str, Vec<(&'a str, &'a str)>),
+}
 
 
 
 fn main() {
-    let (args, response) = crate::args::setup();
+    let unparsed_file = fs::read_to_string("playdate.luars").expect("cannot read file");
 
-    let document = scraper::Html::parse_document(&response);
-    let outer = Selector::parse(concat!(
-        "div.sect1>div.sectionbody>div.sect2>div.item",
-        ",div.sect1>div.sectionbody>div.sect2>div.sect3>div.item",
-        ",div.sect1>div.sectionbody>div.sect2>div.sect3>div.sect4>div.item",
-        ",div.sect1>div.sectionbody>div.sect2>div.sect3>div.sect4>div.sect5>div.item",
-    )).unwrap();
-    let sel_title = Selector::parse("div.title").unwrap();
-    let sel_content = Selector::parse("div.content").unwrap();
-
-    let sel_docs = Selector::parse(
-        concat!(
-            "div.paragraph", ",",
-            "div.ulist", ",",
-            "div.admonitionblock", ",",
-            "div.literalblock", ",",
-            "div.listingblock"
-        )
-    ).unwrap();
-    let sel_docs_text = Selector::parse("p").unwrap();
-    let sel_docs_list = Selector::parse("ul>li").unwrap();
-    let sel_docs_coderay = Selector::parse("code").unwrap();
-    let sel_docs_admonition = Selector::parse("table>tbody>tr>td.content").unwrap();
-
-    let re_function = Regex::new(r"^(?:[\w][\w\d]*\.)*(?:[\w][\w\d]*)(?:[:.][\w][\w\d]*)").unwrap();
-
-    let mut _poop = 0;
-    let mut _last_class: String = "".to_string();
-    let mut stubs : Vec<Stub> = Vec::new();
+    let document = LuarsParser::parse(Rule::Document, &unparsed_file)
+    .expect("unsuccessful parse")
+    .next().unwrap();
 
 
-    for element in document.select(&outer) {
-        _poop = _poop + 1;
-
-        let anchor: &str = element.value().attr("id").unwrap_or("");
-        let title: String = element.select(&sel_title).next().unwrap().text().collect::<String>();
-        // if anchor == "" { eprintln!("WARN: Docs missing anchor for: {}", title); }
-
-        let mut text: Vec<String> = Vec::new();
-        for c in element.select(&sel_content) {
-            // Main Paragraphs of text documentation
-            for div in c.select(&sel_docs) {
-                let dv = div.value();
-                if dv.has_class("paragraph", CaseSensitivity::CaseSensitive) {
-                    div.select(&sel_docs_text).for_each(|p| {
-                        let t = clean_text(p.text().collect::<String>());
-                        // println!("p: {}", t);
-                        text.push(t);
-                    });
-                } else if dv.has_class("listingblock", CaseSensitivity::CaseSensitive) {
-                    text.push("```".to_string());
-                    div.select(&sel_docs_coderay).for_each(|d| {
-                        let lines = clean_code(d.text().collect::<String>());
-                        for l in lines {
-                            text.push(l);
-                        }
-                    });
-                    text.push("```".to_string());
-                } else if dv.has_class("ulist", CaseSensitivity::CaseSensitive) {
-                    div.select(&sel_docs_list).for_each(|li| {
-                        let mut t: String = clean_text(li.text().collect::<String>());
-                        t.insert_str(0, "* ");
-                        text.push(t);
-                    });
-                } else if dv.has_class("admonitionblock", CaseSensitivity::CaseSensitive) {
-                    div.select(&sel_docs_admonition).for_each(|td| {
-                        let adm = fixes::clean_text(td.inner_html());
-                        text.push(adm);
-                    });
-                } else if dv.has_class("literalblock", CaseSensitivity::CaseSensitive) {
-                    div.select(&sel_content).for_each(|d| {
-                        // TODO: This clobbers line breaks. pre should get ``` fencing.
-                        let litb = fixes::clean_text(d.inner_html());
-                        text.push(litb);
-                    });
-                } else {
-                    eprintln!("skipping {:?}", div.value())
-                }
+    for line in document.into_inner() {
+        let f = match line.as_rule() {
+            Rule::Module => {
+                let name = line.into_inner().next().unwrap().as_str();
+                LuarsStatement::Module(name)
             }
-        }
-        // // This gets rid of the brackets (optional functional parameters) in the title
-        // if re_optional.is_match(&title) {
-        //     title = re_brackets.replace_all(&title, "").to_string();
-        // }
-
-        if title.contains("  ") { // Functions with multiple
-            for t in title.split("  ") {
-                let stub = annotate_function(&anchor.to_string(), &t.trim().to_string(), &text);
-                stubs.push(stub)
+            Rule::Constant => {
+                let mut iterator = line.into_inner();
+                // println!("{:#?}", line.into_inner());
+                let cons_name: &str = iterator.next().unwrap().as_str();
+                let cons_type: &str = iterator.next().unwrap().as_str();
+                let cons_value: isize = iterator.next().unwrap().as_str().parse::<isize>().unwrap();
+                LuarsStatement::Constant(cons_name, cons_type, cons_value)
+                // println!("{}", cons_name);
+                // unreachable!()
+                // for part in line.into_inner() {
+                //     let cons_name: &str;
+                //     let cons_type: &str;
+                //     let cons_value: &str;
+                //     match part.as_rule() {
+                //         Rule::Identifier => {
+                //             let cons_name = part.as_str();
+                //             println!("Name: {}", cons_name);
+                //         }
+                //         Rule::ConsType => {
+                //             let cons_type = part.as_str();
+                //             println!("Type: {}", cons_type);
+                //         }
+                //         Rule::IntegerValue => {
+                //             let cons_value = part.as_str();
+                //             println!("Value: {}", cons_value);
+                //         }
+                //         _ => unreachable!(),
+                //     }
+                //     LuarsStatement::Constant(cons_name, cons_type, cons_value)
+                // }
+                // unreachable!()
             }
-        // title.contains("Callback") is workaround for keyboard callbacks.
-        } else if title.contains("(") || title.contains("[") || title.contains(" ") || title.starts_with("-") || title.contains("Callback") { //
-            // function(), imagetable[n], "p + p", "-v", etc
-            let stub = annotate_function(&anchor.to_string(), &title, &text);
-            stubs.push(stub);
-        } else {
-            // TODO: Add this as a stub!
-            // eprintln!("VARIABLE: {title}");
-        }
+            // Rule::Variable => {
+            //     println!("Variable!");
+            // }
+            // Rule::Function => {
+            //     println!("Function!");
+            // }
+            // Rule::Object => {
+            //     println!("Object!");
+            // }
+            // _ => {
+            //     println!("Rule: {:?}", record.as_rule(), record.);
+            // }
+            _ => unreachable!(),
+        };
+        println!("{:?}", f);
+    }
+    // println!("Number of records: {}", statement_count);
 
-        // _last_class is context for the next loop. So if the title is missing a name (e.g. "p + p") we can infer it.
-        if re_function.is_match(&title) && title.contains(":") {
-            _last_class = title.split(":").next().unwrap_or("").to_string();
-        }
-    }
-    let prefix = "---@meta\n-- This file contains function stubs for autocompletion. DO NOT include it in your game.\n";
-    let suffix = "--- End of LuaCATS stubs.";
-
-    if true {
-        for stub in stubs {
-            println!("{}", stub.to_luars());
-        }
-    }
-    else
-    if args.action == Action::Annotate {
-        print_class_defs();
-        println!("{prefix}");
-        for stub in stubs {
-            println!("{}", stub.to_lua());
-        }
-        println!("{suffix}")
-    } else if args.action == Action::Stub {
-        println!("{prefix}");
-        for stub in stubs {
-            println!("{}", stub.to_stub());
-        }
-        println!("{suffix}")
-    }
+    // let successful_parse = LuarsParser::parse(Rule::Statement, "mod json;");
+    // println!("{:?}", successful_parse);
 }
